dba1815cf7b71675944c6315ab811e3212522b91
Kolanar <kola_nar@mail.ru>
2023-03-29 17:43:09 +0300

    Добавленна страница про визуализацию данных

--- Data visualization/mathlib.py
+++ Data visualization/mathlib.py
@@ -1,4 +1,5 @@
 from matplotlib import pyplot as plt
+plt.rcParams['axes.facecolor'] = 'none'
 
 years = [1950,1960,1970,1980,1990,2000,2010] # Годы
 gdp = [300.2,543.3,1075.9,2862.5,5979.6,10289.7,14958.3] # ВВП
